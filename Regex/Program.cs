72b294f0d308cf009d9c12645b1455f99478cf6a
UserPC <UserPC@DESKTOP-5JSVERC>
2023-03-02 18:15:03 +0300

    Добавьте файлы проекта.

---
+++ Regex/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string Time = "Завтрак в 09:00";
+        Regex regex = new Regex("([0-1][0-9]|2[0-3]):[0-5][0-9]");
+        MatchCollection matches = regex.Matches(Time);
+        if (matches.Count > 0)
+        {
+            foreach (Match match in matches)
+                Console.WriteLine(match.Value);
+        }
+        else
+        {
+            Console.WriteLine("Время некорректно");
+        }
+        
+    }
+}
